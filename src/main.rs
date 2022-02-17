88a974a3ce91079ea71d30013f6fdf04743af02c
danielcarney96 <dancarney@live.co.uk>
2022-02-17 21:05:47 +0000

    Use a serial buffer to output results to console, also hide qemu.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,22 @@
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
 }
@@ -43,14 +55,14 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     
     for test in tests {
         test();
